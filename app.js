7431384bb4bd3194b5e8f19d79d6f6ff947749f6
dzulfi <dzulfiqar.zahid13@gmail.com>
2025-05-24 17:00:58 +0700

    materi 3

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var flash = require('express-flash');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/post', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
